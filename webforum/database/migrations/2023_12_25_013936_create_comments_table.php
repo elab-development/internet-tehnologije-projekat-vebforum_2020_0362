c432cdcd5f9eacf6a21737276504e85d05f2b802
Isidora Gacesa <ixi.gacesa@gmail.com>
2023-12-25 02:45:38 +0100

    Napravljene cetiri migracije: -jedna za brisanje kolone email_verified_at iz users tabele -ostale tri  za kreiranje tabela threads, posts i comments

---
+++ webforum/database/migrations/2023_12_25_013936_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('text',150);
+            $table->date('dateOfCreation');
+            $table->int('numberOfLikes');
+            $table->int('numberOfDislikes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
